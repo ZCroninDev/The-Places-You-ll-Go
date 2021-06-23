5cba8d3b739fb9eb759968597b494a776734042f
zach <ZCroninDev@users.noreply.github.com>
2021-06-23 12:32:37 -0400

    Adds xternal scrripts link, creates mouse over effect of images

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function MouseRollover(ramona_winter) {
+    ramona_winter.src = "./assets/ramona-winter.png";
+  }
+    function MouseOut(ramona_spring) {
+    ramona_spring.src = "./assets/ramona-spring.png";
+  }
