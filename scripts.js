8e145fdded84591ede7ce88fa6f9669364b5850e
zach <ZCroninDev@users.noreply.github.com>
2021-06-23 14:19:03 -0400

    Adds new images, creates rollovers

--- scripts.js
+++ scripts.js
@@ -1,6 +1,24 @@
-function MouseRollover(ramona_winter) {
-    ramona_winter.src = "./assets/ramona-winter.png";
+function MouseRolloverOne(ramona_winter) {
+    ramona_winter.src = "./assets/ramona_winter.png";
   }
-    function MouseOut(ramona_spring) {
-    ramona_spring.src = "./assets/ramona-spring.png";
+function MouseOutOne(ramona_spring) {
+    ramona_spring.src = "./assets/ramona_spring.png";
+  }
+function MouseRolloverTwo(lolopass_oregon) {
+    lolopass_oregon.src = "./assets/lolopass_oregon.png";
+  }
+function MouseOutTwo(mcneilpoint_oregon) {
+    mcneilpoint_oregon.src = "./assets/mcneilpoint_oregon.png";
+  }
+function MouseRolloverThree(zionpark_utah) {
+  zionpark_utah.src = "./assets/zionpark_utah.png";
+  }
+function MouseOutThree(thenarrows_utah) {
+  thenarrows_utah.src = "./assets/thenarrows_utah.png";
+  }  
+function MouseRolloverFour(yosemite_fall) {
+  yosemite_fall.src = "./assets/yosemite_fall.png";
+  }
+function MouseOutFour(yosemite_california) {
+  yosemite_california.src = "./assets/yosemite_california.png";
   }
