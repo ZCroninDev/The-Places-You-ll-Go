71580f0aff4c31a2b1265092b948e6bb45831461
zach <ZCroninDev@users.noreply.github.com>
2021-06-30 21:57:49 -0400

    adds carousel images, javascript WIP

--- scripts.js
+++ scripts.js
@@ -46,3 +46,31 @@ function MouseOutSeven(denali_alaska) {
 function MouseOutEight(kalalau_hawaii) {
     kalalau_hawaii.src = "./assets/kalalau_hawaii.png";
   } 
+
+
+  // foreign countries carousel
+
+  var slideIndex = 1;
+  showSlides(slideIndex);
+  
+  // Next/previous controls
+  function plusSlides(n) {
+    showSlides(slideIndex += n);
+  }
+  
+  // Thumbnail image controls
+  function currentSlide(n) {
+    showSlides(slideIndex = n);
+  }
+  
+  function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  }
+  
