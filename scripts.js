34b57e6501a4c57ec35ef1c134c1dcd88a38d625
zach <ZCroninDev@users.noreply.github.com>
2021-07-01 21:40:21 -0400

    Finishes foreign countries js carousel

--- scripts.js
+++ scripts.js
@@ -48,29 +48,17 @@ function MouseOutEight(kalalau_hawaii) {
   } 
 
 
-  // foreign countries carousel
+var slideIndex = 0;
+showSlides();
 
-  var slideIndex = 1;
-  showSlides(slideIndex);
-  
-  // Next/previous controls
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
-  
-  // Thumbnail image controls
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
-  
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("slides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-  }
-  
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 6000); 
+}
